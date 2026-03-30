72d6c505ce2804fb0068fa50e17393d144608e35
sachinstha9 <sachinshrestha7055@gmail.com>
2026-03-30 19:04:24 +1300

    model

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class Qnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(4, 128),
+            nn.ReLU(),
+            nn.Linear(128, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 5)
+        )
+
+    def forward(self, x):
+        return self.net(x)
